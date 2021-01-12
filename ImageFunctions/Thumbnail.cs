55bb16d97cbc4cc9d8a73244fade5747edde2bcf
Michal@SFB2 <michal.marusan@gmail.com>
2021-01-12 21:17:51 +0100

    add SAS token generation

--- ImageFunctions/Thumbnail.cs
+++ ImageFunctions/Thumbnail.cs
@@ -7,6 +7,7 @@
 // Use for local testing:
 //   https://{ID}.ngrok.io/runtime/webhooks/EventGrid?functionName=Thumbnail
 
+using Azure.Storage;
 using Azure.Storage.Blobs;
 using Microsoft.Azure.EventGrid.Models;
 using Microsoft.Azure.WebJobs;
@@ -24,14 +25,18 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-// using Azure.Data.Tables;
-// using Azure.Storage.
+using Azure.Storage.Sas;
+
 
 namespace ImageFunctions
 {
     public static class Thumbnail
     {
         private static readonly string BLOB_STORAGE_CONNECTION_STRING = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+        private static readonly string BLOB_STORAGE_NAME = Environment.GetEnvironmentVariable("AzureWebJobsStorageName");
+        private static readonly string BLOB_STORAGE_KEY = Environment.GetEnvironmentVariable("AzureWebJobsStorageKey");
+        private static readonly string BLOB_STORAGE_CONTAINER = Environment.GetEnvironmentVariable("THUMBNAIL_CONTAINER_NAME");
+        private static readonly int BLOB_SAS_LENTGTH = Int32.Parse(Environment.GetEnvironmentVariable("AzureWebJobsStorageSASTokenLenghtHours"));
         
         private static string GetBlobNameFromUrl(string bloblUrl)
         {
@@ -40,6 +45,51 @@ namespace ImageFunctions
             return blobClient.Name;
         }
 
+        private static string GenSASToken(string blobName)
+        {
+
+            // Create a URI to the storage account
+            Uri accountUri = new Uri("https://" + BLOB_STORAGE_NAME + ".blob.core.windows.net/");
+
+            // Create BlobServiceClient from the account URI
+            BlobServiceClient blobServiceClient = new BlobServiceClient(accountUri);
+
+            // Get reference to the container
+            BlobContainerClient container = blobServiceClient.GetBlobContainerClient(BLOB_STORAGE_CONTAINER);
+
+            string sasBlobUri = "";
+
+            if (container.Exists())
+            {
+                // Set the expiration time and permissions for the container.
+                // In this case, the start time is specified as a few 
+                // minutes in the past, to mitigate clock skew.
+                // The shared access signature will be valid immediately.
+                BlobSasBuilder sas = new BlobSasBuilder
+                {
+                    Resource = "c",
+                    BlobContainerName = BLOB_STORAGE_CONTAINER,
+                    StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
+                    ExpiresOn = DateTimeOffset.UtcNow.AddHours(BLOB_SAS_LENTGTH)
+                };
+
+                sas.SetPermissions(BlobContainerSasPermissions.All);
+
+                // Create StorageSharedKeyCredentials object by reading
+                // the values from the configuration (appsettings.json)
+                StorageSharedKeyCredential storageCredential =
+                    new StorageSharedKeyCredential(BLOB_STORAGE_NAME, BLOB_STORAGE_KEY);
+
+                // Create a SAS URI to the storage account
+                UriBuilder sasUri = new UriBuilder(accountUri);
+                sasUri.Query = sas.ToSasQueryParameters(storageCredential).ToString();
+
+
+                // Create the URI using the SAS query token.
+                sasBlobUri = container.Uri + "/" + blobName + sasUri.Query;
+            }
+                return sasBlobUri;
+        }
 
 
 
@@ -81,12 +131,10 @@ namespace ImageFunctions
                                     RowKey = blobName,
                                     ShareFileName = blobName,
                                     ShortLink = tmp_short_url,
-                                    SASLink = createdEvent.Url,
+                                    SASLink = GenSASToken(blobName),
                                 };
                     
-                    TableService.AddObject(table, newlink);
-
-              
+                    TableService.AddObject(table, newlink);              
                 }
             }
             catch (Exception ex)
